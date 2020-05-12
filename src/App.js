24b6d8c6b800283a45cab9b46ada3ffa725aa30b
GusGo <gus.gonzalez.15@gmail.com>
2020-05-11 20:20:11 -0400

    react router, layount of app

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home";
+import Projects from "./pages/Projects"
 import Wrapper from "./components/Wrapper";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Header from "./components/Header";
+// import PageContainer from "./components/PageContainer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
-      {/* <div> */}
-      {/* <Header /> */}
-      <Wrapper>
-        <Route exact path="/" component={Home} />
-        {/* <Route exact path="/about" component={About} />
-          <Route exact path="/search" component={Search} />  */}
-      </Wrapper>
-      {/* <Footer /> */}
-      {/* </div> */}
+      <div>
+        <Header />
+        <Wrapper>
+          {/* <PageContainer> */}
+          <Route exact path="/" component={Home} />
+          <Route exact path="/projects" component={Projects} />
+          {/* <Route exact path="/connect" component={Connect} /> */}
+          {/* </PageContainer> */}
+        </Wrapper>
+        <Footer />
+      </div>
     </Router>
   );
 }
