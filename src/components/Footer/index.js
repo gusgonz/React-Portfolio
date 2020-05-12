0daa3e99194afa404af849fc00f6c780d2bc71b5
GusGo <gus.gonzalez.15@gmail.com>
2020-05-11 20:20:55 -0400

    footer init

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,2 +1,20 @@
 import React from "react";
-import './index.css';
+import './style.css';
+// import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+
+
+function Footer() {
+    return (
+        <footer className="footer">
+            {/* <Navbar expand="lg" variant="dark" bg="dark" sticky="bottom"> */}
+            <Container fluid id="footer-container">
+
+            </Container>
+            {/* </Navbar> */}
+        </footer>
+    );
+
+}
+
+export default Footer;
