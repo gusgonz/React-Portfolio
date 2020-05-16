b3f60ad5eb01c05559931439e5238ca4dae58626
GusGo <gus.gonzalez.15@gmail.com>
2020-05-16 17:15:48 -0400

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,24 +2,80 @@ import React from "react";
 import './style.css';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-
+import { Link, animateScroll as scroll } from 'react-scroll';
 
 function Header() {
+
+    const url = window.location.href;
+
+    // true = home page, false = contact page
+    const page = !url.includes("contact");
+
+    const scrollOptions = {
+        spy: true,
+        smooth: true,
+        offset: -76,
+        duration: 1000
+    }
+
     return (
         <header>
-            <Navbar fixed="top" expand="md" bg="dark" variant="dark" id="nav">
+            <Navbar fixed="top" expand="md" id="nav">
                 <Navbar.Brand href="/" id="name">Gus Gonzalez</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" >
-                    <Nav className="justify-content-end" style={{ width: "100%" }}>
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/projects">Projects</Nav.Link>
-                        <Nav.Link href="/connect">Connect</Nav.Link>
+                    <Nav className="justify-content-end nav-links" style={{ width: "100%" }}>
+                        {/* Home link to react router */}
+                        {
+                            page
+                                ?
+                                <Navbar.Text className="nav-links" onClick={scroll.scrollToTop}>Home</Navbar.Text>
+                                :
+                                <Nav.Link className="nav-links" href="/">Home</Nav.Link>
+                        }
+                        {/* About scroll link */}
+                        {
+                            page
+                                ?
+                                (<Link to="about" spy={scrollOptions.spy} smooth={scrollOptions.smooth} offset={scrollOptions.offset} duration={scrollOptions.duration} >
+                                    <Navbar.Text className="nav-links" href="">About</Navbar.Text>
+                                </Link>)
+                                :
+                                ""
+                        }
+                        {/* Projects scroll link */}
+                        {
+                            page
+                                ?
+                                (<Link to="projects" spy={scrollOptions.spy} smooth={scrollOptions.smooth} offset={scrollOptions.offset} duration={scrollOptions.duration} >
+                                    <Navbar.Text className="nav-links" href="/projects">Projects</Navbar.Text>
+                                </Link>)
+                                :
+                                ""
+                        }
+                        {/* Connect scroll link */}
+                        {
+                            page
+                                ?
+                                (<Link to="connect" spy={scrollOptions.spy} smooth={scrollOptions.smooth} offset={scrollOptions.offset} duration={scrollOptions.duration} >
+                                    <Navbar.Text className="nav-links" href="/connect">Connect</Navbar.Text>
+                                </Link>)
+                                :
+                                ""
+                        }
+                        {/* Contact link to react router */}
+                        {
+                            page
+                                ?
+                                (<Nav.Link className="nav-links" href="/contact">Contact</Nav.Link>)
+                                :
+                                ""
+                        }
+
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
         </header>
-
     );
 };
 
