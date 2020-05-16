55728f3de683f3039e45c35ad34155ad87a06f41
GusGo <gus.gonzalez.15@gmail.com>
2020-05-16 17:16:43 -0400

    home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,23 +6,36 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import PageContainer from "../../components/PageContainer";
+import ProjectContainer from "../../components/ProjectContainer";
+import Project from "../../components/Project";
+import projects from "../../utils/projects";
+import resume from "../../utils/RESUME.pdf"
+import { Element } from 'react-scroll';
 
 
 function Home() {
+
+    const projectComponents = projects.map(proj => (
+        <Project {...proj} />
+    ));
+
+
     return (
         <PageContainer>
+
             <div id="home-images">
-                <Image src="images/atlanta.jpg" fluid />
-                <Image src="images/headshot.jpg" roundedCircle id="mepic" />
+                <Image src="images/atlanta.jpg" style={{ opacity: .8 }} fluid />
+                <Image src="images/headshot.jpg" roundedCircle id="me" />
             </div>
             <Jumbo>
                 <Container >
-                    <Row>
-                        <h1 id="aboutme">About me</h1>
-                        <Col lg>
+                    <Row className="home-rows">
+                        <Col xs={12} md={4}>
+                            <Element name="about"><h1 >About me</h1></Element>
+                        </Col>
+                        <Col xs={12} md={8}>
 
-
-                            <div id="about-me-text">
+                            <div >
                                 <p>
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem viverra aliquet eget. Purus viverra accumsan in nisl nisi scelerisque. Quam id leo in vitae turpis massa sed elementum tempus. Nibh mauris cursus mattis molestie a. Et odio pellentesque diam volutpat commodo sed egestas egestas. Lectus nulla at volutpat diam ut venenatis.
                                     </p>
@@ -36,6 +49,42 @@ function Home() {
 
                     </Row>
 
+                    <Row className="project-rows">
+                        <Col xs={12} md={4}>
+                            <Element name="projects"><h1 >Projects</h1></Element>
+                        </Col>
+
+                        <Col xs={12} md={12} >
+                            <ProjectContainer>
+                                <Row>
+                                    <Col md={6} >{projectComponents[0]}</Col>
+                                    <Col md={6} lg={6}>{projectComponents[1]}</Col>
+                                </Row>
+                                <Row>
+                                    <Col md={6} lg={6}>{projectComponents[2]}</Col>
+                                    <Col md={6} lg={6}>{projectComponents[3]}</Col>
+                                </Row>
+                                <Row>
+                                    <Col md={6} lg={6}>{projectComponents[4]}</Col>
+                                    <Col md={6} lg={6}>{projectComponents[5]}</Col>
+                                </Row>
+                            </ProjectContainer>
+
+                        </Col>
+                    </Row>
+
+                    <Row className="home-rows">
+                        <Col xs={12} md={5} id="nopadding">
+                            <Element name="connect"><h1 >Connect</h1></Element>
+                        </Col>
+                        <Col xs={12} md={7}>
+                            <Row><p><a className="connect-links" href="/contact">Email: gus.gonzalez.15@gmail.com</a></p></Row>
+                            <Row><p>Phone Number: (786) 564-1133</p></Row>
+                            <Row><p>GitHub: <a className="connect-links" href="https://www.github.com/gusgonz/">https://www.github.com/gusgonz/</a></p></Row>
+                            <Row><p>LinkedIn: <a className="connect-links" href="https://www.linkedin.com/in/gusgonz/">https://www.linkedin.com/in/gusgonz/</a></p></Row>
+                            <Row><p>Resume: <a className="connect-links" href={resume} target="_blank" rel="noopener noreferrer">Open in new tab here</a></p></Row>
+                        </Col>
+                    </Row>
                 </Container>
 
             </Jumbo>
