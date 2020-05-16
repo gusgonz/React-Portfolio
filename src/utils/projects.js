719294aa3412c304d93803702b9fadf681c0f829
GusGo <gus.gonzalez.15@gmail.com>
2020-05-16 17:17:05 -0400

    projects

---
+++ src/utils/projects.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        name: "Music Is For Me",
+        tech: "MongoDB, Express, Node.js, React",
+        desc: "A test node app",
+        gh: "https://github.com/gusgonz/Music-Is-For-Me",
+        deploy: "https://music-is-for-me.herokuapp.com/",
+        image: "images/music.png"
+    },
+    {
+        name: "Test",
+        tech: "Node.js",
+        desc: "A test node app",
+        gh: "https://www.github.com/gusgonz",
+        deploy: "https://www.heroku.com",
+        image: "https://www.placecage.com/200/120"
+    },
+    {
+        name: "Test",
+        tech: "Node.js",
+        desc: "A test node app",
+        gh: "https://www.github.com/gusgonz",
+        deploy: "https://www.heroku.com",
+        image: "https://www.placecage.com/200/120"
+    },
+    {
+        name: "Test",
+        tech: "Node.js",
+        desc: "A test node app",
+        gh: "https://www.github.com/gusgonz",
+        deploy: "https://www.heroku.com",
+        image: "https://www.placecage.com/200/120"
+    },
+    {
+        name: "Test",
+        tech: "Node.js",
+        desc: "A test node app",
+        gh: "https://www.github.com/gusgonz",
+        deploy: "https://www.heroku.com",
+        image: "https://www.placecage.com/200/120"
+    },
+    {
+        name: "Test",
+        tech: "Node.js",
+        desc: "A test node app",
+        gh: "https://www.github.com/gusgonz",
+        deploy: "https://www.heroku.com",
+        image: "https://www.placecage.com/200/120"
+    }
+]
