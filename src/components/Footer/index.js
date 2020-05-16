0d3c396fbd35ae26a50a919ebce2aaf9a686d264
GusGo <gus.gonzalez.15@gmail.com>
2020-05-16 17:15:21 -0400

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import './style.css';
 // import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import Icon from "../../components/Icon";
 
 
 function Footer() {
     return (
         <footer className="footer">
-            {/* <Navbar expand="lg" variant="dark" bg="dark" sticky="bottom"> */}
             <Container fluid id="footer-container">
-
+                <Icon url="https://www.github.com/gusgonz/" alt="GitHub" image="images/github-logo.svg" />
+                <Icon url="https://www.linkedin.com/in/gusgonz/" alt="LinkedIn" image="images/linkedin-logo.svg" />
             </Container>
-            {/* </Navbar> */}
         </footer>
     );
 
