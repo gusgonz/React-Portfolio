ab8da1765bfcb5ddd52da743c38371b29d329345
GusGo <gus.gonzalez.15@gmail.com>
2020-05-16 17:14:55 -0400

    app js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home";
-import Projects from "./pages/Projects"
+import Contact from "./pages/Contact";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
-// import PageContainer from "./components/PageContainer";
 import Footer from "./components/Footer";
 
 function App() {
+
   return (
     <Router>
-      <div>
-        <Header />
-        <Wrapper>
-          {/* <PageContainer> */}
-          <Route exact path="/" component={Home} />
-          <Route exact path="/projects" component={Projects} />
-          {/* <Route exact path="/connect" component={Connect} /> */}
-          {/* </PageContainer> */}
-        </Wrapper>
-        <Footer />
-      </div>
+      <Header />
+      <Wrapper>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/contact" component={Contact} />
+      </Wrapper>
+      <Footer />
     </Router>
   );
+
 }
 
 export default App;
