59c1272562d9653156c140a32674fb126ef26099
GusGo <gus.gonzalez.15@gmail.com>
2020-05-16 17:16:35 -0400

    contact page

---
+++ src/pages/Contact/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css';
+import PageContainer from "../../components/PageContainer";
+import EmailForm from "../../components/EmailForm";
+
+function Contact() {
+    return (
+        <PageContainer>
+            <EmailForm>
+            </EmailForm>
+        </PageContainer>
+    );
+};
+
+export default Contact;
